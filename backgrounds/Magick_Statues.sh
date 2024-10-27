6a837908131e2b6003988f038c7e697f8f03aed8
Mac Mini <macmini@mini27075.local>
2024-10-27 00:29:14 -0400

    A start for #27.  Needs more text alignment.

---
+++ backgrounds/Magick_Statues.sh
@@ -0,0 +1,19 @@
+
+
+URL="https://upload.wikimedia.org/wikipedia/commons/3/3d/Front_view_of_Statue_of_Liberty_with_pedestal_and_base_2024.jpg"
+INPUT="Front_view_of_Statue_of_Liberty_with_pedestal_and_base_2024.jpg"
+
+OUTPUT1="Statue_of_Liberty_LoG_0x2.png"
+OUTPUT2="Statue_of_Liberty_DoG_0_1_5.png"
+
+OUTPUT1b="Statue_of_Liberty_LoG_0x2_LIGHT.png"
+OUTPUT2b="Statue_of_Liberty_DoG_0_1_5_LIGHT.png"
+
+wget ${URL} .
+
+#magick ${INPUT} -bias 1% -morphology Convolve LoG:0x2 ${OUTPUT1}
+
+magick ${INPUT} -bias 0% -morphology Convolve DoG:0,1.0,5.0 ${OUTPUT2}
+
+#magick convert -negate ${OUTPUT1} ${OUTPUT1b}
+magick convert -negate ${OUTPUT2} ${OUTPUT2b}
